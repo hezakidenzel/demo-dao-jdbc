013e3c7e368a2aab43cde08492aa552206068670
Hezaki <hezakidenzel@gmail.com>
2020-03-02 20:30:03 -0300

    Seller add classe

--- src/aplicaçao/Programa.java
+++ src/aplicaçao/Programa.java
@@ -1,13 +1,19 @@
 package aplicaçao;
 
+import java.util.Date;
+
 import model.entidade.Department;
+import model.entidade.Seller;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1,"Books");
+		
+		Seller seller = new Seller(21,"bob","bob@gmail.com",new Date(),3000.0,obj);
 		System.out.println(obj);
+		System.out.println(seller);
 
 	}
 
