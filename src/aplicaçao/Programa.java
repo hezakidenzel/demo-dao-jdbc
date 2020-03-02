b1dc4e0d3da86d83a0506fd89eb1fc0843449f3d
Hezaki <hezakidenzel@gmail.com>
2020-03-02 20:22:45 -0300

    Criando Departamento classe

---
+++ src/aplicaçao/Programa.java
@@ -0,0 +1,14 @@
+package aplicaçao;
+
+import model.entidade.Department;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Department obj = new Department(1,"Books");
+		System.out.println(obj);
+
+	}
+
+}
