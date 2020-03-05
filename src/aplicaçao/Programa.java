9f5340880f0d0299e45e65be853d1c821becb161
Hezaki <hezakidenzel@gmail.com>
2020-03-05 12:13:22 -0300

    Criando o finbyID e Melhorando codigo

--- src/aplicaçao/Programa.java
+++ src/aplicaçao/Programa.java
@@ -2,6 +2,8 @@ package aplicaçao;
 
 import java.util.Date;
 
+import model.Dao.DaoFactory;
+import model.Dao.SellerDao;
 import model.entidade.Department;
 import model.entidade.Seller;
 
@@ -9,12 +11,13 @@ public class Programa {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1,"Books");
-		
-		Seller seller = new Seller(21,"bob","bob@gmail.com",new Date(),3000.0,obj);
-		System.out.println(obj);
-		System.out.println(seller);
 
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		System.out.println("=== Test 1 seller findbyid===");
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
 	}
 
 }
